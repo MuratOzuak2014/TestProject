b050b807a6e5d30c77e83232c5a37d7f68716a89
user <user@192.168.10.135>
2014-01-10 17:14:03 +0200

    initialise commit

--- src/com/test/Main1.java
+++ src/com/test/Main1.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.test;
 
 /**
@@ -11,12 +10,82 @@ package com.test;
  * @author user
  */
 public class Main1 {
-    
+
     public static void main(String[] args) {
-        Pamuk hammadde = new Pamuk ();
+
+        String[] strDizi = new String[3];
+        strDizi[0] = "Arda Uslu";
+        strDizi[1] = "Sinan Aleksander Özüak";
+        strDizi[2] = "Murat Özüak";
+
+        /*int i=100;
         
-        IplikMakinesi makine = new IplikMakinesi ();
-        Iplik iplik = makine.pamuguIplikYap(hammadde);
+         if(i==100){
+         System.out.println("Eşit");
+         } else {
+         System.out.println("Eşit değil");
+                
+         }*/
+        /*for(int i=0;i<strDizi.length;i++) {
+         if(strDizi[i].equals("Arda Uslu"))
+         System.out.println(strDizi[i]);
+         }*/
+        /*for(int i=1; i<=100; i++) {
+         System.out.println(i);
+         }*/
+        /*while(i<100) {
+         System.out.println(i);
+         i++
+         }*/
+        /*int i=1; 
+         do{
+         System.out.println(i);
+         i++
+                    
+         }while (i<100)*/
+        int ay = 1;
+        switch (ay) {
+            case 1:
+                System.out.println("Ocak");
+                break;
+            case 2:
+                System.out.println("Şubat");
+                break;
+            case 3:
+                System.out.println("Mart");
+                break;
+            case 4:
+                System.out.println("Nisan");
+                break;
+            case 5:
+                System.out.println("Mayıs");
+                break;
+            case 6:
+                System.out.println("Haziran");
+                break;
+            case 7:
+                System.out.println("Temmuz");
+                break;
+            case 8:
+                System.out.println("Ağustos");
+                break;
+            case 9:
+                System.out.println("Eylül");
+                break;
+            case 10:
+                System.out.println("Ekim");
+                break;
+            case 11:
+                System.out.println("Kasım");
+                break;
+            case 12:
+                System.out.println("Aralık");
+                break;
+            default:
+                System.out.println("Yanlış Ay değeri");
+                break;
+
+        }
     }
-    
+
 }
