0fb577175e4c07bf3cf6989d066c0c7278129144
user <user@user-Bilgisayar>
2014-01-15 18:22:01 +0200

    initialise commit

---
+++ src/com/test/sinan/NameUtils.java
@@ -0,0 +1,13 @@
+package com.test.sinan;
+
+public class NameUtils {
+  public static String randomFirstName() {
+    int num = (int)(Math.random()*1000);
+    return("John" + num);
+  }
+  
+  public static String randomLastName() {
+    int num = (int)(Math.random()*1000);
+    return("Smith" + num);
+  }
+}
---
+++ src/com/test/sinan/Person4.java
@@ -0,0 +1,21 @@
+package com.test.sinan;
+
+public class Person4 {
+  public String firstName;
+  public String lastName;
+  
+  public Person4() {
+  }
+  public Person4(String firstName) {
+        this.firstName = firstName;
+  }
+    
+  public Person4(String firstName, String lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+  }
+  
+  public String getFullName() {
+    return(firstName + " " + lastName);
+  }
+}
